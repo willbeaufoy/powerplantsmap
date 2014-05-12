545cb7869f6ceb6b779b1ef3424cd74a67ca5ce3
wtrb <will@willbeaufoy.net>
2014-05-12 22:34:20 +0100

    Made change location zoom

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -21,27 +21,4 @@ $(document).ready(function() {
         }
         $('ul.l3').toggleClass("hide")
     })
-
-   //  $(':checkbox.all-country-filter').change(function() {
-//         console.log(this.checked)
-//         if(this.checked) {
-//             $(':checkbox.continent-filter').prop("checked", true)
-//             $(':checkbox.country-filter').prop("checked", true)
-//         }
-//         else {
-//             $(':checkbox.continent-filter').prop("checked", false)
-//             $(':checkbox.country-filter').prop("checked", false)
-//         }
-//     })
-// 
-//     $(':checkbox.all-type-filter').change(function() {
-//         console.log(this.checked)
-//         if(this.checked) {
-//             $(':checkbox.type-filter').prop("checked", true)
-//         }
-//         else {
-//             $(':checkbox.type-filter').prop("checked", false)
-//         }
-// })
-
 })
